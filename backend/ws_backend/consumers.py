f719d898295b7d3343fa8597c4f155a7e2b4cd8b
shinyfuwante <mr.philliptang@gmail.com>
2023-12-21 23:33:15 -0800

    Start implementing Game class

--- backend/ws_backend/consumers.py
+++ backend/ws_backend/consumers.py
@@ -1,18 +1,38 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .core.game_logic import Game
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # Called when the WebSocket is handshaking
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        self.group_name = f'game_{self.game_id}'
+        self.game = Game(self.game_id)
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
         await self.accept()
 
         # Implement session handling or game setup logic here
 
     async def disconnect(self, close_code):
-        # Called when the WebSocket closes
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
         pass
 
-    async def receive(self, text_data):
-        # Called when the consumer receives a message from the WebSocket
-        pass
+    async def receive_json(self, content):
+        game_state = await self.update_game_state(content)
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'game_state',
+                'game_state': game_state
+            }
+        )
+    
+    async def game_state(self, event):
+        await self.send_json(event['game_state'])
 
     # Implement methods to handle incoming WebSocket messages and manage game sessions
