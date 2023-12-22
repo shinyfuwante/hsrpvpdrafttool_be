f7de0cf2e226d718a2d717b3df9c77233bf2a2e3
shinyfuwante <mr.philliptang@gmail.com>
2023-12-22 00:16:58 -0800

    Handle incoming test message

--- backend/ws_backend/consumers.py
+++ backend/ws_backend/consumers.py
@@ -1,7 +1,8 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from .core.game_logic import Game
+import json
 
-class GameConsumer(AsyncWebsocketConsumer):
+class GameConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         # Called when the WebSocket is handshaking
         self.game_id = self.scope['url_route']['kwargs']['game_id']
@@ -21,18 +22,33 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         pass
+    
+    async def receive(self, text_data):
+        try:
+            content = json.loads(text_data)
+        except json.JSONDecodeError:
+            print(f"Invalid JSON: {text_data}")
+            return
 
+        await self.receive_json(content)
+        
     async def receive_json(self, content):
-        game_state = await self.update_game_state(content)
+        print('received_json')
+        game_state = await self.game_state(content)
+        # publishes to Redis channel
         await self.channel_layer.group_send(
             self.group_name,
             {
-                'type': 'game_state',
-                'game_state': game_state
+                'type': 'game_state', # determines what method to call.. 
+                'game_state': 'test',
+                'sender_channel_name': self.channel_name
             }
         )
+        pass
     
     async def game_state(self, event):
+        print('received game_state message')
+        print(event)
         await self.send_json(event['game_state'])
 
     # Implement methods to handle incoming WebSocket messages and manage game sessions
