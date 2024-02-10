0c18864b19dee00b0df249afc7f7492f131e5efe
shinyfuwante <mr.philliptang@gmail.com>
2024-02-09 16:52:08 -0800

    Swap to RedisPubSubChannelLayer to fix occasional message drop

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = "backend.asgi.application"
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
         },
